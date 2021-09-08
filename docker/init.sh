07742a1520e6f6efeb2a4d554db9b57103812623
Sanaul Hasan <61028774+sanaulhasan@users.noreply.github.com>
2021-09-08 22:47:40 +0500

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "<h1> Welcome! This App is running on $(hostname) with IP address: $(hostname -i)  </h1>" > /usr/share/nginx/html/index.html
+echo "<h1> Welcome!! This App is running on $(hostname) with IP address: $(hostname -i)  </h1>" > /usr/share/nginx/html/index.html
