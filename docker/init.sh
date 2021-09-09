0c358ab6a6436b5dc25d3364e0d9a11c57ec0626
Sanaul Hasan <61028774+sanaulhasan@users.noreply.github.com>
2021-09-10 00:31:50 +0500

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -2,3 +2,4 @@
 
 echo "<h1> Welcome!! This App is running on $(hostname) with IP address: $(hostname -i)  </h1>" > /usr/share/nginx/html/index.html
 echo "<h1> Application upgraded version 3.  </h1>" >> /usr/share/nginx/html/index.html
+echo "<h1> Thanks all for joining!!  </h1>" >> /usr/share/nginx/html/index.html
